2ca74950ef03b80002d5ef3d4677477cd3d1d09e
Tobias <tobiashorschmg@gmail.com>
2026-04-17 17:36:17 +0200

    Add more scopes to setup

--- src/scripts/setup.ts
+++ src/scripts/setup.ts
@@ -751,7 +751,24 @@ export async function runSetup(): Promise<void> {
           context.setNextStep('oauth-token');
 
         } else if (method === 'manual') {
-          const authUrl = getOAuthAuthorizationUrl(clientId, redirectUri, environment);
+          const authScopes = [
+            'https://api.ebay.com/oauth/api_scope',
+            'https://api.ebay.com/oauth/api_scope/sell.analytics.readonly',
+            'https://api.ebay.com/oauth/api_scope/sell.marketing',
+            'https://api.ebay.com/oauth/api_scope/sell.inventory.readonly',
+            'https://api.ebay.com/oauth/api_scope/sell.inventory',
+            'https://api.ebay.com/oauth/api_scope/sell.account.readonly',
+            'https://api.ebay.com/oauth/api_scope/sell.account',
+            'https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly',
+            'https://api.ebay.com/oauth/api_scope/sell.fulfillment',
+            'https://api.ebay.com/oauth/api_scope/sell.marketing.readonly',
+            'https://api.ebay.com/oauth/api_scope/sell.reputation.readonly',
+            'https://api.ebay.com/oauth/api_scope/sell.reputation',
+            'https://api.ebay.com/oauth/api_scope/sell.finances',
+            'https://api.ebay.com/oauth/api_scope/commerce.identity.readonly',
+            'https://api.ebay.com/oauth/api_scope/commerce.message'
+          ];
+          const authUrl = getOAuthAuthorizationUrl(clientId, redirectUri, environment, authScopes);
           context.showNote('OAuth Authorization URL', authUrl);
           await context.openBrowser(authUrl);
           showInfo('1. Sign in to your eBay account in the browser');
